75a5f65e7c644976ee56cde6dabc97d0a3e0f63b
wulongjiang <562445380@qq.com>
2022-11-25 15:54:15 +0800

    feat: 将tailwind转成unocss

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,4 +1,11 @@
-import type { AttributifyAttributes } from '@unocss/preset-attributify'
+/*
+ * @Author: wlj
+ * @Date: 2022-11-25 10:33:01
+ * @LastEditors: wlj
+ * @LastEditTime: 2022-11-25 15:53:01
+ * @Description:
+ */
+import type { AttributifyAttributes } from 'unocss/preset-attributify';
 
 declare module 'react' {
   interface HTMLAttributes<T> extends AttributifyAttributes {}
